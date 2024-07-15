9e60ddfacd890694fe18513faae8c3638c485be1
Ismail Demir <ismailhuseyinn53@gmail.com>
2024-07-15 15:47:13 +0300

    React

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -7,37 +7,37 @@ const apiClient = axios.create({
     },
 });
 
-// Ürünleri al
+
 export const getProducts = async () => {
     const response = await apiClient.get('/product');
     return response.data;
 };
 
-// Ürün oluþtur
+
 export const createProduct = async (product) => {
     const response = await apiClient.post('/product', product);
     return response.data;
 };
 
-// Ürün güncelle
+
 export const updateProduct = async (id, product) => {
     const response = await apiClient.put(`/product/${id}`, product);
     return response.data;
 };
 
-// Ürün sil
+
 export const deleteProduct = async (id) => {
     await apiClient.delete(`/product/${id}`);
 };
 
-// Ürün arama
+
 export const searchProducts = async (keyword) => {
     const response = await apiClient.get(`/product/search?keyword=${keyword}`);
     return response.data;
 };
 
-// Ürün grubu oluþtur
+
 export const createProductGroup = async (group) => {
-    const response = await apiClient.post('/productgroup', group); // Doðru endpointi kullandýðýndan emin ol
+    const response = await apiClient.post('/productgroup', group); 
     return response.data;
 };
